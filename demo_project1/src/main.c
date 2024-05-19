ea6ec55c6cb6272b13978e15e019a81427a5a75d
giang.nguyen-hoang <giang.nguyen-hoang@banvien.com.vn>
2024-05-19 18:13:53 +0700

    c1: practice Makefile

---
+++ demo_project1/src/main.c
@@ -0,0 +1,10 @@
+
+
+#include "main.h"
+
+int main(void){
+
+	printf("%d + %d = %d\n", 1, 2, 1+2);
+	printf("sum(2,3) = %d\n",sum(2,3));
+return 0;
+}
---
+++ test7 automatic variables/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#define pi 3.1415
+
+int main(void)
+{
+printf("hello I'm G\n");
+
+return 0;
+}
